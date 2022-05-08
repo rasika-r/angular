30329a7b0554d393c536fef11332147e25c75e56
rasika-r <105144130+rasika-r@users.noreply.github.com>
2022-05-08 12:05:13 +0530

    Add files via upload

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+  submit(login: any){
+    console.log('submitted',login);
+    
+  }
+}
